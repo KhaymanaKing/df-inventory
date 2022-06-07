4dfdd888062ec8b790544722d97a79fa1065ce01
Khayman King <khaymanak@gmail.com>
2022-06-07 11:21:29 -0700

    started building out skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react/cjs/react.production.min';
 import './App.css';
+import SignIn from './Auth/SignIn';
+import SignUp from './Auth/SignUp';
+import InventoryForm from './InventoryForm/InventoryForm';
+import InventoryItem from './InventoryItem/InventoryItem';
+import InventoryList from './InventoryList/InventoryList';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header> Add Hamberder Menu</header>
+      <SignIn/>
+      <SignUp/>
+      <InventoryForm/>
+      <InventoryList/>
+      <InventoryItem/>
     </div>
   );
 }
